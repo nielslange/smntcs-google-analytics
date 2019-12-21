6d05c6d35906d09ddeffb7b4abff86c7fd615158
Niels Lange <info@nielslange.de>
2019-12-21 12:50:31 +0800

    Fix Travis issue

--- smntcs-google-analytics.php
+++ smntcs-google-analytics.php
@@ -19,19 +19,32 @@
  * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
  */
 
-// Avoid direct plugin access
+/**
+ * Avoid direct plugin access
+ *
+ * @since 1.0.0
+ */
 if ( ! defined( 'ABSPATH' ) ) {
 	die( '¯\_(ツ)_/¯' );
 }
 
-// Load text domain
-add_action( 'plugins_loaded', 'smntcs_google_analytics_load_textdomain' );
+/**
+ * Load text domain
+ *
+ * @since 1.0.0
+ */
 function smntcs_google_analytics_load_textdomain() {
 	load_plugin_textdomain( 'smntcs-google-analytics', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
+add_action( 'plugins_loaded', 'smntcs_google_analytics_load_textdomain' );
 
-// Add Adobe Typekit Fonts to WordPress Customizer
-add_action( 'customize_register', 'smntcs_google_analytics_register_customize' );
+/**
+ * Add Adobe Typekit Fonts to WordPress Customizer
+ *
+ * @param WP_Customize_Manager $wp_customize The instance of WP_Customize_Manager.
+ * @return void
+ * @since 1.0.0
+ */
 function smntcs_google_analytics_register_customize( $wp_customize ) {
 	$wp_customize->add_section(
 		'smntcs_google_analytics_section',
@@ -74,19 +87,30 @@ function smntcs_google_analytics_register_customize( $wp_customize ) {
 		)
 	);
 }
+add_action( 'customize_register', 'smntcs_google_analytics_register_customize' );
 
-// Add settings link on plugin page
-add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'smntcs_ga_plugin_settings_link' );
-function smntcs_ga_plugin_settings_link( $links ) {
+
+/**
+ * Add settings link on plugin page
+ *
+ * @param string $url The original URL.
+ * @return string $url The updated URL.
+ * @since 1.0.0
+ */
+function smntcs_ga_plugin_settings_link( $url ) {
 	$admin_url     = admin_url( 'customize.php?autofocus[control]=smntcs_google_analytics_tracking_code' );
 	$settings_link = '<a href="' . $admin_url . '">' . __( 'Settings', 'smntcs-google-analytics' ) . '</a>';
-	array_unshift( $links, $settings_link );
+	array_unshift( $url, $settings_link );
 
-	return $links;
+	return $url;
 }
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'smntcs_ga_plugin_settings_link' );
 
-// Load Adobe Typekit Fonts code and custom CSS
-add_action( 'wp_footer', 'smntcs_google_analytics_enqueue' );
+/**
+ * Load Adobe Typekit Fonts code and custom CSS
+ *
+ * @since 1.0.0
+ */
 function smntcs_google_analytics_enqueue() {
 	if ( ! is_admin() && get_option( 'smntcs_google_analytics_tracking_code' ) ) {
 		$tracking = get_option( 'smntcs_google_analytics_tracking_code' );
@@ -97,3 +121,4 @@ function smntcs_google_analytics_enqueue() {
 		print( $tracking ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 }
+add_action( 'wp_footer', 'smntcs_google_analytics_enqueue' );
